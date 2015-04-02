cf583375568d201fca498aec0b8dd4f1603a84f3
TonyFeng <340114550@qq.com>
2015-04-02 02:19:24 -0400

    initial version
    extend Bootstrap/Build HomePage with slideshow

---
+++ phase4/themes/bootstrap/views/site/index.php
@@ -0,0 +1,86 @@
+<?php
+$this->pageCaption='';
+$this->pageTitle=Yii::app()->name;
+?>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+<!--           <ol class="carousel-indicators"> -->
+<!--             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li> -->
+<!--             <li data-target="#carousel-example-generic" data-slide-to="1"></li> -->
+<!--             <li data-target="#carousel-example-generic" data-slide-to="2"></li> -->
+<!--           </ol> -->
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+    	<div class="peopleCarouselImg">
+      		<img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/coworking1.jpg" alt="...">
+      	</div>
+      <div class="carousel-caption">
+      	<center><h1>Discover the power of collaborative productivity</h1></center>
+      </div>
+    </div>
+    <div class="item">
+    	<div class="peopleCarouselImg">
+      		<img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/coworking2.jpg" alt="...">
+      	</div>
+      <div class="carousel-caption">
+      	<center><h1>Explore ideas with a passionate, like-minded community</h1s></center>
+      </div>
+    </div>
+	<div class="item">
+		<div class="peopleCarouselImg">
+      		<img src="<?php echo Yii::app()->theme->baseUrl; ?>/img/coworking3.jpg" alt="...">
+      	</div>
+      <div class="carousel-caption">
+      	<center><h1>Reimagine and reinspire the future of working</h1></center>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only"></span>
+  </a>
+</div>
+
+<hr style="border:0;background-color:#000000;height:1px;">
+      <section id="video" class="video">
+        <div class="video-container">
+            <h1 calign="center"> Your coworking space rental and teaming to succeed destination</h1>
+            </br>
+            <iframe height="400" width="700" src="https://www.youtube.com/embed/39FkG1Lgt24?rel=0" frameborder="0" allowfullscreen></iframe>
+        </div>
+        </br></br></br>
+        <div class="row">
+            <div class="col-md-6">
+              <div class="intro_word" style="color: #6E6E6E">
+                <h3>JOIN AN INCREDIBLE SOCIAL NETWORK</h3>
+                <blockquote>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                </blockquote>
+                  <h3>REASONABLE RATES</h3>
+                <blockquote>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                </blockquote>
+              </div>
+            </div>
+            <div class="col-md-6">
+                <div class="intro_word" style="color: #6E6E6E">
+                  <h3>OPEN SPACE MEANS OPEN MINDS</h3>
+                  <blockquote>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                  </blockquote>
+                  <h3>ACCESS TO AMENITIES</h3>
+                  <blockquote>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
+                  </blockquote>
+                </div>
+            </div>
+        </div>
+      </section>
